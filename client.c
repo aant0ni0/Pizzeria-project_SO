008f2109b0b9b9ff10d0780142773e4353a06d1e
Antoni Michalczak <antoni0michalczak@gmail.com>
2025-01-04 19:57:08 +0100

    Add client logic for receiving responses and handling seating
    - Implemented message receiving with `msgrcv` and error handling.
    - Added logic to handle seating: client exits if no table is available or simulates eating if seated.

--- client.c
+++ client.c
@@ -33,5 +33,22 @@ int main(int argc, char* argv[]){
         perror("Błąd: Nie udało się wysłać wiadomości do kolejki komunikatów za pomocą msgsnd. Upewnij się, że kolejka istnieje i że proces ma odpowiednie uprawnienia.");
         exit(EXIT_FAILURE);
     }
+
+    struct msgbuf_response resp;
+    if(msgrcv(msgid, &resp, sizeof(resp) - sizeof(long), 2 , 0) == -1){
+        perror("Błąd: Nie udało się odebrać wiadomości z kolejki komunikatów za pomocą msgrcv. Upewnij się, że kolejka istnieje, jest dostępna oraz że proces ma odpowiednie uprawnienia.");
+        exit(EXIT_FAILURE);
+    }
+
+    if(!resp.canSit){
+        printf("[KLIENT %d] Brak wolnego miejsca. Wychodzę.\n", getpid());
+        return 0;
+    }
+    else{
+        printf("[KLIENT %d] Otrzymałem stolik %d-osobowy. Jem przez %d sekund...\n", getpid(), resp.tableSize, eatTime);
+        sleep(eatTime);
+        printf("[KLIENT %d] Skończyłem, wychodzę.\n", getpid());
+    }
 }
+
 
