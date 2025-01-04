f7fe6a828bb476eab480537c7c9ed0817136037c
Antoni Michalczak <antoni0michalczak@gmail.com>
2025-01-04 14:29:32 +0100

    Add semaphore management functions for process synchronization
    - Implemented `create_semaphore`: Creates a semaphore and initializes its value to 1 for mutual exclusion.
    - Implemented `get_semaphore`: Retrieves the identifier of an existing semaphore using the provided IPC key.
    - Implemented `semaphore_down`: Performs the down (P) operation on the semaphore, blocking the process if the semaphore value is 0.
    - Implemented `semaphore_up`: Performs the up (V) operation on the semaphore, increasing its value and allowing blocked processes to proceed.

--- ipc_helpers.h
+++ ipc_helpers.h
@@ -5,4 +5,10 @@
 
 
 int create_msg_queue(key_t key);
+
 int create_semaphore(key_t key);
+int get_semaphore(key_t key);
+void remove_semaphore(int semid);
+
+void semaphore_down(int semid);
+void semaphore_up(int semid);
